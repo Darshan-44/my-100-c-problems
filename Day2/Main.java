0f596b26cbba00c024cee43081e931c1e97c2a1f
darshanG-123 <upliftedmind2006@gmail.com>
2025-07-23 22:50:42 +0530

    Add files via upload

---
+++ Day2/Main.java
@@ -0,0 +1,13 @@
+package bios;
+
+
+public class Main {
+	public static void main(String[]args) {
+		Double principal=100000.0 , rate=3.0 ,  time=3.0 ;
+		Double interest = (principal*rate*time)/100 ;
+		System.out.println("Interest after 3 years: "+ interest);
+	
+		
+		}
+
+}
---
+++ Day2/Main2.java
@@ -0,0 +1,15 @@
+package bios;
+
+
+
+public class Main2 {
+public static void main(String[]args) {
+	int a=10;
+	int b=3;
+	int Remainder=a%b;
+	System.out.println("Remainder after dividing "+Remainder);
+
+	
+	
+}
+}
