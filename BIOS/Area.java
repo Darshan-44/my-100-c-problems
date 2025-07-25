bbfd5008a1c49f86a8a5bb5f196dd70fe9e134ac
Darshan.G <upliftedmind2006@gmail.com>
2025-07-25 10:02:08 +0530

    Add files via upload

---
+++ BIOS/Area.java
@@ -0,0 +1,21 @@
+package bios;
+import java.util.Scanner;
+
+public class q4 {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the length: ");
+		Double length = sc.nextDouble();
+		System.out.println("Enter the breadth: ");
+		Double breadth = sc.nextDouble();
+		Double area= length*breadth;
+		System.out.println("Area of rectangle "+ area);
+		sc.close();
+		
+	
+		
+		
+	}
+	
+
+}
