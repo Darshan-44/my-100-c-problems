219eae149df0194e4e1437c0483bd80dbff76235
Darshan.G <upliftedmind2006@gmail.com>
2025-07-26 13:54:34 +0530

    Add files via upload

---
+++ Farenheit.java
@@ -0,0 +1,16 @@
+package bios;
+import java.util.Scanner;
+
+public class q5 {
+	public static void main(String[]args) {
+		Scanner sc= new Scanner (System.in);
+		System.out.println("Enter the celcius value: ");
+		Double celcius= sc.nextDouble();
+		Double fahrenheit= (celcius*9/5)+32 ;
+		System.out.println("Temperature in Fahrenheit: "+fahrenheit);
+		
+		sc.close();
+		
+	}
+
+}
---
+++ Name.java
@@ -0,0 +1,17 @@
+package bios;
+import java.util.Scanner;
+
+public class q3 {
+	public static void main(String[]args) {
+    Scanner sc = new Scanner (System.in )	;
+    System.out.print("Enter the name : ");
+    String name = sc.nextLine();
+    System.out.print("Namah Shivaya "+  name +"!");
+    
+    
+    sc.close();
+    
+	}
+
+}
+
